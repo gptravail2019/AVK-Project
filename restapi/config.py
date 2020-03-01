ec2be56bd94c6fe1bb2bac8f3e112d616af6a94e
Bedreddine Zarrouk <baderzarrouk@gmail.com>
2020-03-01 18:37:36 +0100

    crud category

--- restapi/config.py
+++ restapi/config.py
@@ -1,4 +1,6 @@
+import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "mysql://root:root@0.0.0.0:3307/avk"
+SQLALCHEMY_DATABASE_URI = 'mysql://avk:avk2019@0.0.0.0:3307/avk'
